f698453478ca33aaed3a59c2c083f9cc90cfbe8f
Harik-Sodhi <92112555+Harik-Sodhi@users.noreply.github.com>
2023-12-08 16:09:36 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,6 @@
 
 using namespace pros;
 using namespace std;
-void screen() {
-    // loop forever
-    while (true) {
-        lemlib::Pose pose = chassis.getPose(); // get the current position of the robot
-        pros::lcd::print(0, "x: %f", pose.x); // print the x position
-        pros::lcd::print(1, "y: %f", pose.y); // print the y position
-        pros::lcd::print(2, "heading: %f", pose.theta); // print the heading
-        pros::delay(10);
-    }
-}
 /**
  * A callback function for LLEMU's center button.
  *
@@ -37,56 +27,63 @@ void on_center_button() {
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
+Controller master(E_CONTROLLER_MASTER);
+pros::Motor catapult (3, E_MOTOR_GEARSET_36, false);
+pros::Motor lhs_1 (17,E_MOTOR_GEARSET_06,false); //port, internal gearing (1=green,2=blue), reverse
+pros::Motor lhs_2 (20,E_MOTOR_GEARSET_06,false);
+pros::Motor lhs_3 (19,E_MOTOR_GEARSET_06,true);
+pros::Motor_Group Leftdrive ({lhs_1,lhs_2,lhs_3});
+pros::Motor rhs_1 (7,E_MOTOR_GEARSET_06,true); 
+pros::Motor rhs_2 (5,E_MOTOR_GEARSET_06,true);
+pros::Motor rhs_3 (6,E_MOTOR_GEARSET_06,false);
+pros::Motor_Group Rightdrive ({rhs_1,rhs_2,rhs_3});
+pros::Motor blocker(2, E_MOTOR_GEARSET_18, false);
+pros::Motor intake(10, E_MOTOR_GEARSET_18, false);
+lemlib::Drivetrain_t drivetrain {
+	&Leftdrive, // left drivetrain motors
+	&Rightdrive, // right drivetrain motors
+	10, // track width
+	3.25, // wheel diameter
+	360 // wheel rpm
+};
+bool intakeOn = false; 
+pros::Imu inertial_sensor(9);
+lemlib::OdomSensors_t sensors {nullptr,nullptr,nullptr,nullptr,&inertial_sensor};
+lemlib::ChassisController_t lateralController {
+	8, // kP
+	30, // kD
+	1, // smallErrorRange
+	100, // smallErrorTimeout
+	3, // largeErrorRange
+	500, // largeErrorTimeout
+	5 // slew rate
+};
+ 
+// turning PID
+lemlib::ChassisController_t angularController {
+	4, // kP
+	40, // kD
+	1, // smallErrorRange
+	100, // smallErrorTimeout
+	3, // largeErrorRange
+	500, // largeErrorTimeout
+	40 // slew rate
+};
+ 
+ 
+// create the chassis
+lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
+
 void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
 
 	pros::lcd::register_btn1_cb(on_center_button);
-	Controller master(E_CONTROLLER_MASTER);
-	pros::Motor lhs_1 (2,E_MOTOR_GEARSET_06,false); //port, internal gearing (1=green,2=blue), reverse
-  	pros::Motor lhs_2 (1,E_MOTOR_GEARSET_06,false);
-	pros::Motor lhs_3 (4,E_MOTOR_GEARSET_06,true);
-  	pros::Motor_Group Leftdrive ({lhs_1,lhs_2,lhs_3});
-	pros::Motor rhs_1 (14,E_MOTOR_GEARSET_06,true); 
-  	pros::Motor rhs_2 (15,E_MOTOR_GEARSET_06,true);
-	pros::Motor rhs_3 (3,E_MOTOR_GEARSET_06,false);
-  	pros::Motor_Group Rightdrive ({rhs_1,rhs_2,rhs_3});
-	lemlib::Drivetrain_t drivetrain {
-    	&Leftdrive, // left drivetrain motors
-    	&Rightdrive, // right drivetrain motors
-    	10, // track width
-    	3.25, // wheel diameter
-    	360 // wheel rpm
-	};
-	pros::Imu inertial_sensor(2);
 	inertial_sensor.reset();
-	lemlib::OdomSensors_t sensors {&inertial_sensor};
-	lemlib::ChassisController_t lateralController {
-    	8, // kP
-    	30, // kD
-    	1, // smallErrorRange
-    	100, // smallErrorTimeout
-    	3, // largeErrorRange
-    	500, // largeErrorTimeout
-    	5 // slew rate
-	};
- 
-	// turning PID
-	lemlib::ChassisController_t angularController {
-    	4, // kP
-    	40, // kD
-    	1, // smallErrorRange
-    	100, // smallErrorTimeout
-    	3, // largeErrorRange
-    	500, // largeErrorTimeout
-    	40 // slew rate
-	};
  
  
 	// create the chassis
-	lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
 	chassis.setPose(0, 0, 0); // X: 0, Y: 0, Heading: 0
-	pros::Task screenTask(screen);
 }
 
 /**
@@ -119,7 +116,7 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
-	chassis.moveTo(-10, 0, 1000); // drive PID tuning
+	chassis.moveTo(-10, 0, 1000); // drive PID tunin
 	//chassis.turnTo(0,-10,1000); // turn PID tuning
 }
 
@@ -131,13 +128,38 @@ void autonomous() {
  *
  * If no competition control is connected, this function will run immediately
  * following initialize().
- *
+ *a
  * If the robot is disabled or communications is lost, the
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	while true{
-		Leftdrive.move(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y)^3 * 27/10^6)
+	while (true) {
+		Leftdrive.move(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y)^3/127^2);
+		Rightdrive.move(master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y)^3/127^2);
+
+		if (master.get_digital(E_CONTROLLER_DIGITAL_R2) == 1){
+
+			catapult.move(127);
+		};
+		if (master.get_digital(E_CONTROLLER_DIGITAL_L1))
+		{
+			blocker.move(-127);
+		}
+		else{
+			
+		}
+		if (master.get_digital(E_CONTROLLER_DIGITAL_L2))
+		{
+			blocker.move(127);
+		}
+		if (master.get_digital(E_CONTROLLER_DIGITAL_R1))
+		{
+			intakeOn = !intakeOn;
+		}
+		if (intakeOn)
+		{
+			intake.move(127);
+		}
 	}
 }
